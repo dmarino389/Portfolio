d529b025da460c0768a926f1860fdd3b336a54a6
dmarino389 <139820583+dmarino389@users.noreply.github.com>
2023-12-06 02:35:05 -0600

    pu

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -40,7 +40,7 @@ export const Projects = () => {
         <Row>
           <Col>
             <h2>Projects</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione, nostrum nulla. Rerum eos sunt exercitationem beatae excepturi sint voluptate. Unde aliquid animi laboriosam assumenda reiciendis, nobis expedita tempore mollitia consequatur.</p>
+            <p>Lorem ipsum dolor sit amet consectetu, adipisicing elit. Ratione, nostrum nulla. Rerum eos sunt exercitationem beatae excepturi sint voluptate. Unde aliquid animi laboriosam assumenda reiciendis, nobis expedita tempore mollitia consequatur.</p>
           </Col>
         </Row>
       </Container>
