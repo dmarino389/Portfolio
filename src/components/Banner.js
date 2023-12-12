d140bbd668b86e90ffb440a5ed7d9a4c0a238b03
dmarino389 <139820583+dmarino389@users.noreply.github.com>
2023-12-12 16:28:22 -0600

    needs touch up

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,66 +1,75 @@
-import { useState, useEffect } from "react"
-import { Container, Col, Row } from "react-bootstrap"
-import { ArrowRightCircle } from "react-bootstrap-icons"    
-import headerImg from '../assets/img/header-img.svg'
-
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.svg";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
 
 export const Banner = () => {
-  const [ loopNum, setLoopNum ] = useState(0);
-  const [ isDeleting, setIsDeleting ] = useState(false);
-  const toRotate = ['web developer', 'web designer', 'Creative', 'UI/UX Designer']
-  const [ text, setText ] = useState('');
-  const [ delta, setDelta ] = useState(50);
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
   const period = 2000;
 
   useEffect(() => {
     let ticker = setInterval(() => {
-        tick();
+      tick();
     }, delta);
 
-    return () => clearInterval(ticker);
-}, [text]);
+    return () => { clearInterval(ticker) };
+  }, [text])
 
-const tick = () => {
-  let i = loopNum % toRotate.length;
-  let fullText = toRotate[i];
-  let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
 
-  setText(updatedText);
+    setText(updatedText);
 
-  if (isDeleting) {
-    setDelta(50); // Rapid deletion from the start
-  } else {
-    setDelta(150); // Typing speed (adjust as needed)
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
   }
 
-  if (!isDeleting && updatedText === fullText) {
-    setIsDeleting(true);
-    setTimeout(() => setDelta(50), 2000); // Wait for a bit before starting deletion
-  } else if (isDeleting && updatedText === '') {
-    setIsDeleting(false);
-    setLoopNum(loopNum + 1);
-  }
-}
-
-
-  
   return (
-    <section className="banner" id = 'home'>
+    <section className="banner" id="home">
       <Container>
-        <Row className='align-items-center'>
-          <Col xs = {12} md = {6} xl = {7}>
-            <span className = 'tagline'>Welcome to my Portfolio</span>
-            <h1>
-  {'Hi! My name is Dimitrius and I\'m a '}
-  <br />
-  <span className="wrap">{text}</span>
-</h1>
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <span className="tagline">Welcome to my Portfolio</span>
+                <h1>{`Hi! I'm Dimitrius`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
+                <p>Being the son of first-generation Italian Americans, I learned the importance of hard work and respect from a young age. As a child, I've always had an interest in technology and the different ways in which it impacts the world. Through software development, I have been granted the opportunity to freely explore my passion and hold the ability to create amazing applications from inception to a finished product</p>
 
-            <p>Being the son of first-generation Italian Americans, I learned the importance of hard work and respect from a young age. As a child, I've always had an interest in technology and the different ways in which it impacts the world. Through software development, I have been granted the opportunity to freely explore my passion and hold the ability to create amazing applications from inception to a finished product</p>
-            <button onClick={() => console.log('connect')}> Let's Connect <ArrowRightCircle size={25}/></button>
+                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+              </div>}
+            </TrackVisibility>
           </Col>
-          <Col xs = {12} md = {6} xl = {5}>
-            <img src = {headerImg} alt = "Header Img"/>
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <img src={headerImg} alt="Header Img"/>
+                </div>}
+            </TrackVisibility>
           </Col>
         </Row>
       </Container>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,11 +1,11 @@
-import { Col, Container, Row } from "react-bootstrap";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
-import meter1 from '../assets/img/meter1.svg'
-import meter2 from '../assets/img/meter2.svg'
-import meter3 from '../assets/img/meter3.svg'
-import colorSharp from '../assets/img/color-sharp.png'
-
+import meter1 from "../assets/img/meter1.svg";
+import meter2 from "../assets/img/meter2.svg";
+import meter3 from "../assets/img/meter3.svg";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import arrow1 from "../assets/img/arrow1.svg";
+import arrow2 from "../assets/img/arrow2.svg";
+import colorSharp from "../assets/img/color-sharp.png"
 
 export const Skills = () => {
   const responsive = {
@@ -29,39 +29,36 @@ export const Skills = () => {
   };
 
   return (
-    <section className="skills" id="skills">
-      <Container>
-        <Row>
-          <Col>
-            <div className="skill-bx">
-              <h2>
-                Skills
-              </h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit eligendi quod voluptate, dolor doloribus quia ipsam modi doloremque veritatis placeat! Cum natus eum repellendus molestias dolorem cumque iure amet facere.</p>
-              <Carousel responsive={responsive} infinite = {true} className="skill-slider">
-                <div className="item">
-                  <img src = {meter1} alt="Image" />
-                  <h5>Web Development</h5>
+    <section className="skill" id="skills">
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    <div className="skill-bx wow zoomIn">
+                        <h2>Skills</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
+                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+                            <div className="item">
+                                <img src={meter1} alt="Image" />
+                                <h5>Web Development</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter2} alt="Image" />
+                                <h5>Brand Identity</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter3} alt="Image" />
+                                <h5>Logo Design</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter1} alt="Image" />
+                                <h5>Web Development</h5>
+                            </div>
+                        </Carousel>
+                    </div>
                 </div>
-                <div className="item">
-                  <img src = {meter2} alt="Image" />
-                  <h5>Brand Identity</h5>
-                </div>
-                <div className="item">
-                  <img src = {meter3} alt="Image" />
-                  <h5>Logo Design</h5>
-                </div>
-                <div className="item">
-                  <img src = {meter1} alt="Image" />
-                  <h5>Web Development</h5>
-                </div>
-              </Carousel> 
             </div>
-          </Col>
-        </Row>
-      </Container>
-      <img className="background-image-left" src={colorSharp} />
+        </div>
+        <img className="background-image-left" src={colorSharp} alt="Image" />
     </section>
   )
-
 }
